b1f73e7b5fea7b4b7be9cb23515be92a2d3abc46
Apoorv Singh <singhapo@msu.edu>
2015-04-06 11:13:32 -0400

    fixed regex

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
   validates_presence_of :title, :rating
-  validates :title, :format => { :with => /\A[a-zA-Z\d\s\W]*\z/,
+  validates :title, :format => { :with => /\A[a-zA-Z\d\s\p{Punct}]*\z/,
     message: "only allows letters and numbers" }
   validates :rating, :inclusion => {:in => 0..10, :message => "must be between 0 and 10"}
 end
